2e2ad7c0125c98fb3be66486946686dfb3c53ea6
vinicius-costa-swo <115734967+vinicius-costa-swo@users.noreply.github.com>
2022-12-08 07:37:52 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-nt group creataz deployment group create --resource-group rg-vinicius-costa-demo --template-file main.profiles.bicep
+az deployment group create --resource-group rg-vinicius-costa-demo --template-file main.profiles.bicep
 
-az deploymee --resource-group rg-vinicius-costa-demo --template-file main.backend.bicep
+az deployment group create--resource-group rg-vinicius-costa-demo --template-file main.backend.bicep
 
 az deployment group create --resource-group rg-vinicius-costa-demo --template-file main.bicep
